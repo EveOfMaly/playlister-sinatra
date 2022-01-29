f965e770b26ac2dc4d96f5bffc8ae9d206fe83fe
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-29 09:39:11 -0500

    save

--- db/migrate/20220128205929_create_song_genres.rb
+++ db/migrate/20220128205929_create_song_genres.rb
@@ -2,7 +2,7 @@ class CreateSongGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :song_genres do |t|
       t.integer :song_id
-      t.string :genre_ids
+      t.string :genre_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_28_205929) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.string "genre_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+  end
 
 end
